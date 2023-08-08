d731ff15ea7b020f0c26bd47bd2a59d9c78b4c27
Adit <alifanandityoarifin@gmail.com>
2023-08-08 18:49:11 +0700

    chore: supabase env again

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.SUPABASE_KEY
+const supabaseUrl = 'https://yjcvpwznmzettziwxzgk.supabase.co'
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqY3Zwd3pubXpldHR6aXd4emdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0MTYxMjIsImV4cCI6MjAwNjk5MjEyMn0.6iP18oW66lWDkUUjelW_5OH7EvXJ8BjscvZF2yLP51Y'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
